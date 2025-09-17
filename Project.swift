bff14472ee8f59232eae8fdbe1d070e8cbc8cca5
TomaszLizer <tomasz.lizer@anywhere.co>
2025-09-17 15:40:34 +0200

    Integrate Firebase Crashlytics via Xcode & SPM

--- Project.swift
+++ Project.swift
@@ -1,7 +1,15 @@
 import ProjectDescription
 
+let baseSettings = SettingsDictionary()
+    .debugInformationFormat(.dwarfWithDsym)
+let projectSettings = Settings.settings(base: baseSettings)
+
 let project = Project(
     name: "tuist-firebase",
+    packages: [
+        .remote(url: "https://github.com/firebase/firebase-ios-sdk", requirement: .exact("12.3.0"))
+    ],
+    settings: projectSettings,
     targets: [
         .target(
             name: "tuist-firebase",
@@ -14,13 +22,32 @@ let project = Project(
                         "UIColorName": "",
                         "UIImageName": "",
                     ],
+                    "CFBundleShortVersionString": "$(MARKETING_VERSION)",
+                    "CFBundleVersion": "$(CURRENT_PROJECT_VERSION)",
                 ]
             ),
             buildableFolders: [
                 "tuist-firebase/Sources",
                 "tuist-firebase/Resources",
             ],
-            dependencies: []
+            scripts: [
+                .post(
+                    script: """
+                    ${BUILD_DIR%/Build/*}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/run
+                    """,
+                    name: "Upload Symbols to Crashlytics",
+                    inputPaths: [
+                        "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}",
+                        "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Resources/DWARF/${PRODUCT_NAME}",
+                        "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Info.plist",
+                        "$(TARGET_BUILD_DIR)/$(UNLOCALIZED_RESOURCES_FOLDER_PATH)/GoogleService-Info.plist",
+                        "$(TARGET_BUILD_DIR)/$(EXECUTABLE_PATH)",
+                    ]
+                )
+            ],
+            dependencies: [
+                .package(product: "FirebaseCrashlytics")
+            ]
         ),
         .target(
             name: "tuist-firebaseTests",
